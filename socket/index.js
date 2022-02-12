c511b59238931beefb1103f9e70e65fc77f7ea53
shaakilahammed <shaakilahammed@gmail.com>
2022-02-12 22:32:12 +0600

    send notification  from backend

--- socket/index.js
+++ socket/index.js
@@ -6,11 +6,37 @@ const io = new Server({
   },
 });
 
+let onlineUsers = [];
+
+const addNewUser = (username, socketId) => {
+  !onlineUsers.some((user) => user.username === username) &&
+    onlineUsers.push({ username, socketId });
+};
+
+const removeUser = (socketId) => {
+  onlineUsers = onlineUsers.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (username) => {
+  return onlineUsers.find((user) => user.username === username);
+};
+
 io.on('connection', (socket) => {
-  io.emit('message', 'Hello World');
+  socket.on('newUser', (username) => {
+    addNewUser(username, socket.id);
+    // io.emit('onlineUsers', onlineUsers);
+  });
+
+  socket.on('sendNotification', ({ senderName, receiverName, type }) => {
+    const receiver = getUser(receiverName);
+    io.to(receiver.socketId).emit('getNotification', {
+      senderName,
+      type,
+    });
+  });
 
   socket.on('disconnect', () => {
-    console.log('socket disconnected');
+    removeUser(socket.id);
   });
 });
 
